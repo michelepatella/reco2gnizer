5255ea68b99fa55b27ea093652c9955e88278192
Michele Patella <patellamichele2002@gmail.com>
2026-05-11 15:34:17 +0200

    refactor: remove classes in agent's state

--- src/carbon_react_agent/state.py
+++ src/carbon_react_agent/state.py
@@ -7,51 +7,10 @@ from __future__ import annotations
 
 from collections.abc import Sequence
 from dataclasses import dataclass, field
-from typing import Annotated, Any, Literal, TypedDict
+from typing import Annotated, Any
 
 from langchain_core.messages import AnyMessage
 from langgraph.graph import add_messages
-
-
-class ProductCarbonFootprint(TypedDict):
-    """Structured representation of a Product Carbon Footprint (PCF) value.
-
-    This class defines the expected structure for representing a PCF value.
-
-    Attributes:
-        co2e_kg (float):
-            The carbon footprint value expressed in kilograms of CO2 equivalent.
-
-        source (Literal["official", "estimated"]):
-            The source from which the PCF value was obtained ("official", "estimated").
-
-        explanation (str):
-            Explanation to provide details about how the PCF value was obtained.
-    """
-
-    co2e_kg: float
-
-    source: Literal["official", "estimated"]
-
-    explanation: str
-
-
-class RetrievedEvidence(TypedDict):
-    """Structured representation of retrieved evidence for estimating carbon footprint.
-
-    This class defines the expected structure for representing retrieved evidence.
-
-    Attributes:
-        content (str):
-            The content of the retrieved evidence.
-
-        source (str):
-            The source from which the evidence was obtained.
-    """
-
-    content: str
-
-    source: str
 
 
 @dataclass
@@ -62,7 +21,7 @@ class InputState:
     representing a narrower interface to the outside world.
 
     Attributes:
-        product_data (dict[str, Any]):
+        product_data (Dict[str, Any]):
             The data of the product for which the carbon footprint is being computed.
     """
 
@@ -83,10 +42,10 @@ class State(InputState):
         search_web_count (int):
             Tracks the number of times search web has been called per agent run.
 
-        retrieved_evidence (list[RetrievedEvidence]):
+        retrieved_evidence (List[Any]):
             Stores relevant information gathered for estimating carbon footprint.
 
-        pcf (Optional[ProductCarbonFootprint]):
+        pcf (Optional[dict[str, Any]]):
             The final carbon footprint result.
     """
 
@@ -96,6 +55,6 @@ class State(InputState):
 
     search_web_count: int = field(default=0)
 
-    retrieved_evidence: list[RetrievedEvidence] = field(default_factory=list)
+    retrieved_evidence: list[Any] = field(default_factory=list)
 
-    pcf: ProductCarbonFootprint | None = field(default=None)
+    pcf: dict[str, Any] = field(default={})
