259ccdac5230eff93c21d25a3de4f5bde537cb0c
Michele Patella <patellamichele2002@gmail.com>
2026-05-11 23:05:00 +0200

    refactor: refactor constant names

--- src/carbon_react_agent/const.py
+++ src/carbon_react_agent/const.py
@@ -4,9 +4,7 @@ This module defines constants for the agent.
 """
 
 # Model
-DEFAULT_MODEL = (
-    "google_genai:gemini-2.5-flash"  # Alternatively: "openai:o3-mini"
-)
+DEFAULT_MODEL = "google_genai:gemini-2.5-flash"
 DEFAULT_MODEL_TEMPERATURE = 0.0
 
 # Search web
@@ -14,12 +12,15 @@ DEFAULT_SEARCH_WEB_MAX_RESULTS = 10
 DEFAULT_SEARCH_WEB_TYPE = "auto"
 DEFAULT_SEARCH_WEB_MAX_CALLS = 3
 
+# Graph
+GRAPH_NAME = "carbon-react-agent"
+
 # Graph nodes
-CALL_MODEL_NODE = "call_model"
-TOOLS_NODE = "tools"
-ANSWER_NODE = "answer"
-START_NODE = "__start__"
-END_NODE = "__end__"
+CALL_MODEL_GRAPH_NODE = "call_model"
+TOOLS_GRAPH_NODE = "tools"
+ANSWER_GRAPH_NODE = "answer"
+START_GRAPH_NODE = "__start__"
+END_GRAPH_NODE = "__end__"
 
 # Tools
 SEARCH_WEB_TOOL = "search_web"
